08316e22b14b4eb98c1713dfccb93798c19d3c9a
zdk <nx2zdk@gmail.com>
2011-03-10 14:59:36 +0700

    Add SYNOPSIS POD document to module

--- lib/CatalystX/OAuth2/Provider.pm
+++ lib/CatalystX/OAuth2/Provider.pm
@@ -17,9 +17,75 @@ after 'setup_components' => sub {
 
 =head1 NAME
 
-CatalystX::OAuth2::Provider - 
+CatalystX::OAuth2::Provider -
+
+=head1 SYNOPSIS
+
+    package MyApp;
+    use Moose;
+    use namespace::autoclean;
+
+    use Catalyst qw/
+        +CatalystX::OAuth2::Provider
+        Authentication
+        Session
+        Session::Store::File
+        Session::State::Cookie
+        Session::State::URI
+        Session::State::Auth
+    /;
+
+    extends 'Catalyst';
+
+    __PACKAGE__->config(
+        'Plugin::Authentication' => { # Auth config here }
+    );
+
+    __PACKAGE__->config(
+       'Plugin::Session' => { param => 'code', rewrite_body => 0 }, #Handle authorization code
+    );
+
+    __PACKAGE__->config(
+        'Controller::OAuth' => {
+            login_form => {
+               template => 'user/login.tt',
+               field_names => {
+                   username => 'mail',
+                   password => 'userPassword'
+               }
+            },
+            authorize_form => {
+                template => 'oauth/authorize.tt',
+            },
+            auth_info => {
+                client_1 => {
+                    client_id      => q{THIS_IS_ID},
+                    client_secret  => q{THIS_IS_SECRET},
+                    redirect_uri   => q{CLIENT_REDIRECT_URI},
+                },
+            }
+        }
+    );
+
 
 =head1 DESCRIPTION
+
+CatalystX::OAuth2::Provider is an application class provides a OAuth2 Provider in only
+your Catalyst application configuration.
+
+=head1 REQUIREMENTS
+
+=over
+
+=item A Catalyst application
+
+=item A working Authentication configuration
+
+=item A working Session configuration
+
+=item A View
+
+=back
 
 =head1 METHODS
 
@@ -29,7 +95,7 @@ CatalystX::OAuth2::Provider -
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 the above author(s).
+Copyright 2011 the above author(s).
 
 This sofware is free software, and is licensed under the same terms as perl itself.
 
